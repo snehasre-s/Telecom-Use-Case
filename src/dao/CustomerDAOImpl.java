db420e39d3962cf907bdeb0f862d6a95afd72a2c
Aditya <144247551+adiianand03@users.noreply.github.com>
2025-08-31 15:51:33 +0530

    Create CustomerDAOImpl.java

---
+++ src/dao/CustomerDAOImpl.java
@@ -0,0 +1,76 @@
+package dao;
+
+import model.Customer;
+import model.Invoice;
+import repo.CustomerSeeder;
+
+import java.util.List;
+
+public class CustomerDAOImpl implements CustomerDAO {
+
+    private List<Customer> customers;
+
+    // Constructor loads seeded customers once
+    public CustomerDAOImpl() {
+        // This ensures DAO works with the same list as CustomerSeeder
+        this.customers = CustomerSeeder.seedCustomers();
+    }
+
+    @Override
+    public void registerCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    @Override
+    public List<Customer> listAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Invoice displayInvoice(int customerId) {
+        // to be implemented
+        return null;
+    }
+
+    @Override
+    public void addUsage(int customerId, double data, int voice, int sms) {
+        for (Customer c : customers) {
+            if (c.getCustomerId() == customerId) {
+                c.setOverageCount(c.getOverageCount() + 1);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void updateCustomerDetails(int customerId, String field, String newValue) {
+        for (Customer c : customers) {
+            if (c.getCustomerId() == customerId) {
+                switch (field.toLowerCase()) {
+                    case "name":
+                        c.setName(newValue);
+                        break;
+                    case "phone":
+                        c.setPhone(newValue);
+                        break;
+                    case "email":
+                        c.setEmail(newValue);
+                        break;
+                    case "referral_stat":
+                        c.setReferral_stat(newValue);
+                        break;
+                    case "password":
+                        c.setPassword(newValue);
+                        break;
+                    case "plan_id":
+                        c.setPlan_id(Integer.parseInt(newValue));
+                        break;
+                    case "family_id":
+                        c.setFamily_id(newValue);
+                        break;
+                }
+                break;
+            }
+        }
+    }
+}
